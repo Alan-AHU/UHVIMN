c84c5bb9692e81f014389253e63d64abc5042cbc
Alan-AHU <153062814+Alan-AHU@users.noreply.github.com>
2025-07-29 16:48:27 +0800

    Revert "1"
    This reverts commit d3b7534c5356d578d522f383c97892b84b5057bb.

--- lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from lib.PraNet import PraNet
-from lib.Baseline import Baseline
-from lib.CANet import CANet
-from lib.UACANet import UACANet
--- run/__init__.py
+++
@@ -1,3 +0,0 @@
-from .Train import train
-from .Test import test
-from .Eval import evaluate
